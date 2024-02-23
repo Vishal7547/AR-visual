1262f6172807c3b153a3ef5208796718089f0ea1
Mansi523 <kumarimansi523@gmail.com>
2024-02-22 23:50:00 -0800

    added the pricing page

--- src/pages/Pricing.jsx
+++ src/pages/Pricing.jsx
@@ -10,6 +10,7 @@ import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
+import { IoCheckmark } from "react-icons/io5";
 const Pricing = () => {
   return (
     <div className="container-fluid">
@@ -22,13 +23,13 @@ const Pricing = () => {
             >
               <div className="containercard">
                 <div className="containercarditems mt-5">
-                  <Card sx={{ minWidth: 275 }}>
+                  <Card sx={{ minWidth: 275 }} className="cardpricing w-50 mx-5">
                     <CardContent>
                       <div className="basicardcontent">
                         <span>Basic</span>
                       </div>
-                      <div className="mothhcardcnt">
-                        <span>Free</span>
+                      <div className="paracarde">
+                        <p>Idea for those who've already got their Website up and running and are seeking assistance to enhance and update it further.</p>
                       </div>
                       <div className="mothhcardcnt">
                         <span>1 month</span>
@@ -36,6 +37,28 @@ const Pricing = () => {
                       <div className="rupeecardcnt">
                         <span>₹ 49</span>
                       </div>
+                      <div className="linecard w-75 my-3"></div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>3-5 day turnaround</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Native Development</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Task delivered One-by-one</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Dedicated Dashboard</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Update via Dashboard & Slack</span>
+                      </div>
+                      <div className="linecard w-75 mt-3"></div>
                     </CardContent>
                     <CardActions>
                       <Button size="small">Register</Button>
@@ -43,10 +66,13 @@ const Pricing = () => {
                   </Card>
                 </div>
                 <div className="containercarditems mt-5">
-                  <Card sx={{ minWidth: 275 }}>
+                  <Card sx={{ minWidth: 275 }} className="cardpricing w-75 mx-5">
                     <CardContent>
                       <div className="basicardcontent">
                         <span>Pro Plus</span>
+                      </div>
+                      <div className="paracarde">
+                        <p>Idea if you want to buildor scale your website fast,with the statergy calss included.</p>
                       </div>
                       <div className="mothhcardcnt">
                         <span>6 month</span>
@@ -54,6 +80,34 @@ const Pricing = () => {
                       <div className="rupeecardcnt">
                         <span>₹ 499 </span>
                       </div>
+                      <div className="linecard w-75 my-3"></div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>3-5 day turnaround</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Native Development</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Task delivered One-by-one</span>
+                      </div>
+                      
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Dedicated Dashboard</span>
+                      </div>
+                    
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Task delivered One-by-one</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Dedicated Dashboard</span>
+                      </div>
+                      <div className="linecard w-75 mt-3"></div>
                     </CardContent>
                     <CardActions>
                       <Button size="small">Register</Button>
@@ -61,10 +115,13 @@ const Pricing = () => {
                   </Card>
                 </div>
                 <div className="containercarditems mt-5">
-                  <Card sx={{ minWidth: 275 }}>
+                  <Card sx={{ minWidth: 275 }} className="cardpricing w-50 mx-5">
                     <CardContent>
                       <div className="basicardcontent">
                         <span>Premium</span>
+                      </div>
+                      <div className="paracarde">
+                        <p>If these plans don't fit,let's create one that suits.Customize your subscription for a perfect fit,bigger or smaller!</p>
                       </div>
                       <div className="mothhcardcnt">
                         <span>1 year</span>
@@ -72,6 +129,28 @@ const Pricing = () => {
                       <div className="rupeecardcnt">
                         <span>₹ 1000</span>
                       </div>
+                      <div className="linecard w-75 my-3"></div>
+                            <div className="divcontent">
+                      <IoCheckmark />
+                      <span>3-5 day turnaround</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Native Development</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Task delivered One-by-one</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Dedicated Dashboard</span>
+                      </div>
+                      <div className="divcontent">
+                      <IoCheckmark />
+                      <span>Update via Dashboard & Slack</span>
+                      </div>
+                      <div className="linecard w-75 mt-3"></div>
                     </CardContent>
                     <CardActions>
                       <Button size="small">Register</Button>
